fae7333b8bf3a78214a3c24964aea5372090d342
gurukumara <gurukumara@gmail.com>
2016-08-03 23:49:01 +0530

    Add new test

--- Test.php
+++ Test.php
@@ -5,6 +5,11 @@ class Test extends PHPUnit_Framework_TestCase
 	public function testOnePlusOne() {
 		$this->assertEquals(1+1,2);
   	}
+
+	public function testDoSomething() {
+		n = 10;
+		$this->assertEquals(n*n, 20);
+	}
 }
 
 ?>
